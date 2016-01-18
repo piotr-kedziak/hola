8066060fe6b0dff650074b39728fa6678be8e828
Piotr Kędziak <piotr@kedziak.com>
2016-01-18 14:37:51 +0100

    basic rack framework with basic routes

--- lib/hola.rb
+++ lib/hola.rb
@@ -2,5 +2,39 @@ require 'hola/version'
 
 # doc...
 module Hola
-  # Your code goes here...
+  class Application
+    def call(env)
+      klass, action = controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
+      [200, { 'Content-Type' => 'text/html' }, [text]]
+    end
+
+    def controller_and_action(env)
+      _, controller, action, _after = env['PATH_INFO'].split('/', 4)
+      # default controller and action names
+      controller = 'Home'   if controller.nil? || controller == ''
+      action     = 'index'  if action.nil? || action == ''
+
+      controller = controller.capitalize  # "Home"
+      controller += 'Controller'          # "HomeController"
+
+      [Object.const_get(controller), action]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
+    end
+  end
+
+  # example:....
+  # class HomeController < Hola::Controller
+  #   def index
+  #     'Hola! espanioles!!!!!!'
+  #   end
+  # end
 end
